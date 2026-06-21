87f6703182ac11ee63db30bb7b96c88bcd872b7f
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2026-06-21 17:55:27 +0700

    Добавлена структура для EGTS_SR_TERM_IDENTITY

---
+++ Application/SubRecords/EGTS_SR_TERM_IDENTITY/EGTS_SR_TERM_IDENTITY.go
@@ -0,0 +1,15 @@
+package SubRecords
+
+/*Подзапись для идентификации терминала*/
+type EGTS_SR_TERM_IDENTITY struct {
+	TerminalIdentifier                    uint32
+	Flags                                 EGTS_SR_TERM_IDENTITYFlags
+	HomeDispatcherIdentifier              uint16
+	InternationalMobileEquipmentIdentity  string
+	InternationalMobileSubscriberIdentity string
+	LanguageCode                          string
+	//TODO Разобрать эту запись
+	NetworkIdentifier                                   []byte
+	BufferSize                                          uint16
+	MobileStationIntegratedServicesDigitalNetworkNumber string
+}
---
+++ Application/SubRecords/EGTS_SR_TERM_IDENTITY/EGTS_SR_TERM_IDENTITYFlags.go
@@ -0,0 +1,13 @@
+package SubRecords
+
+/*Флаги для */
+type EGTS_SR_TERM_IDENTITYFlags struct {
+	MobileNetworkExists                         bool
+	BufferSizeExists                            bool
+	NetworkIdentifierExists                     bool
+	SSRA                                        bool
+	LanguageCodeExists                          bool
+	InternationalMobileSubscriberIdentityExists bool
+	InternationalMobileEquipmentIdentityExists  bool
+	HomeDispatcherIdentifierExists              bool
+}
